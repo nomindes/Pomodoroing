97d719af50c69c830e102a4da6b0b853a1c3c15f
nomin <nouminyt@gmail.com>
2024-04-22 14:06:58 +0900

    a

--- lib/screens/timer.dart
+++ lib/screens/timer.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoroing/widgets/customappbar.dart';
 
 class Timer extends StatelessWidget {
   const Timer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: CustomAppBar(),
+    );
   }
 }
---
+++ lib/widgets/customappbar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text("Pomodoroing⌛️"),
+    );
+  }
+}
